0ef2bdf8ffeaa581b02fb6e22b457e03b6cca56b
Nguyen Nghia <nghiavfx16356@funix.edu.vn>
2023-06-06 10:27:02 +0700

    css done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="trello-master">
+      <nav className='navbar app'>App bar</nav>
+      <nav className='navbar board'>Board bar</nav>
+      <nav className='board-columns'>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' />
+              Design & Research
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' />
+              Design & Research
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' />
+              Design & Research
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' />
+              Design & Research
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' />
+              Design & Research
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' />
+              Design & Research
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' />
+              Design & Research
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' />
+              Design & Research
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' />
+              Design & Research
+            </li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className='columns'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png' />
+              Design & Research
+            </li>
+            <li>Second</li>
+            <li>thir</li>
+            <li>Second</li>
+            <li>thir</li>
+            <li>Second</li>
+            <li>thir</li>
+            <li>Second</li>
+            <li>thir</li>
+            <li>Second</li>
+            <li>thir</li>
+            <li>Second</li>
+            <li>thir</li>
+            <li>thir</li>
+            <li>thir</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </nav>
     </div>
   );
 }
